b24b59d478d46a4faf6141f2428defde8a10f0ab
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-05-11 03:15:25 -0700

    Update basisdata-toko-buku.sql

--- basisdata-toko-buku.sql
+++ basisdata-toko-buku.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 10 Bulan Mei 2020 pada 12.28
--- Versi server: 10.4.11-MariaDB
--- Versi PHP: 7.4.4
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2020 at 02:08 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,40 +21,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `toko_buku`
 --
-CREATE DATABASE IF NOT EXISTS `toko_buku` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `toko_buku`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `administrator`
+-- Table structure for table `administrator`
 --
 
 CREATE TABLE `administrator` (
-  `id_admin` varchar(10) NOT NULL,
+  `username` varchar(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `jenis_kelamin` char(10) NOT NULL,
   `alamat` varchar(50) NOT NULL,
-  `tanggal_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` datetime(6) NOT NULL,
   `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `book`
---
-
-CREATE TABLE `book` (
-  `isbn` varchar(20) NOT NULL,
-  `judul_buku` varchar(30) NOT NULL,
-  `pengarang` varchar(50) NOT NULL,
-  `penerbit` varchar(30) NOT NULL,
-  `tahun_terbit` int(11) NOT NULL,
-  `tempat_terbit` varchar(30) NOT NULL,
-  `harga` int(11) NOT NULL,
-  `jumlah_stok` int(11) NOT NULL,
-  `lokasi_penempatan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -61,16 +42,10 @@ CREATE TABLE `book` (
 --
 
 --
--- Indeks untuk tabel `administrator`
+-- Indexes for table `administrator`
 --
 ALTER TABLE `administrator`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indeks untuk tabel `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`isbn`);
+  ADD PRIMARY KEY (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
