f8a37789ef1ca342a86094c45cc37a97d97e9946
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-05-13 08:31:48 -0700

    Update Database.java
    penambahan method tambahData, hapusData, dan updateData

--- BookStore/src/inc/Database.java
+++ BookStore/src/inc/Database.java
@@ -45,7 +45,36 @@ public class Database {
         rs = stm.executeQuery(query);
         return rs;
     }
-    public static void main(String args[]){
-        
+
+    public void tambahData(String isbn, String judul, String penulis, String penerbit, String tahun, String tempat, String harga, String stok, String lokasi) throws SQLException {
+        String query = "INSERT INTO book VALUES ('"+isbn+"', '"+judul+"', '"+penulis+"', '"+
+                penerbit+"', '"+tahun+"', '"+tempat+"', "+harga+", "+stok+", '"
+                +lokasi+"');";
+        stm.execute(query);
     }
+
+    public void hapusData(String isbn) throws SQLException {
+        String query = "DELETE FROM book WHERE isbn = '"+isbn+"'";
+        stm.execute(query);
+    }
+
+    public void updateData(String isbn, String judul, String lokasi, String penerbit, String penulis, String stock, String tahun, String tempat, String harga) throws SQLException {
+        String query = "UPDATE book SET judul_buku = '"+judul+"' WHERE isbn = '"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET lokasi_penempatan = '"+lokasi+"' WHERE isbn = '"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET penerbit = '"+penerbit+"' WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET pengarang = '"+penulis+"' WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET tempat_terbit = '"+tempat+"' WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET tahun_terbit = "+tahun+" WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET harga = "+harga+" WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+        query = "UPDATE book SET jumlah_stok = "+stock+" WHERE isbn='"+isbn+"'";
+        stm.execute(query);
+    }
+
 }
